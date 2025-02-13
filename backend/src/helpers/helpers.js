f6dd6ec13aa3a1eb3d451d33894ecbf421429724
Tomasz Nawrocki <tomasz.nawrocki@codeshine.com>
2025-02-13 10:39:58 +0100

    init java app

--- backend/src/helpers/helpers.js
+++
@@ -1,7 +0,0 @@
-export const validateRequest = (schema, req, res) => {
-  const { error, value } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).json({ error: error.details });
-  }
-  return value;
-};
--- backend/src/types/subscriptions.js
+++
@@ -1,9 +0,0 @@
-/**
- * @typedef {Object} Subscription
- * @property {string} name - Name of the subscription
- * @property {number} monthlyCost - Monthly cost of the subscription
- * @property {'monthly' | 'yearly'} billingCycle - Billing cycle of the subscription
- */
-
-/** @type {Subscription[]} */
-export const subscriptions = [];
