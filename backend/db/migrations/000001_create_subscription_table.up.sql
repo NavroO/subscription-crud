d8b97cdfc22d170c1564661f05090c38b9064a87
NavroO <tomeknawrocki2000@gmail.com>
2025-02-19 11:47:06 +0100

    add connection to database

---
+++ backend/db/migrations/000001_create_subscription_table.up.sql
@@ -0,0 +1,15 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'billing_cycle_type') THEN
+        CREATE TYPE billing_cycle_type AS ENUM ('monthly', 'annually');
+    END IF;
+END$$;
+
+CREATE TABLE IF NOT EXISTS subscription (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    monthly_cost DECIMAL(10, 2) NOT NULL,
+    billing_cycle billing_cycle_type NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
+	"database/sql"
+
 	"github.com/gorilla/mux"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 type Subscription struct {
@@ -12,6 +21,23 @@ type Subscription struct {
 	Name         string `json:"name"`
 	MonthlyCost  string `json:"monthlyCost"`
 	BillingCycle string `json:"billingCycle"`
+}
+
+var db *sql.DB
+
+func connectToDatabase() (*sql.DB, error) {
+	connStr := "user=tomeknawrocki dbname=subscription_crud sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return db, nil
 }
 
 func getSubscription(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +52,43 @@ func getSubscription(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(subscription)
 }
 
+func addSubscription(w http.ResponseWriter, r *http.Request) {
+	// code to add a new subscription
+}
+
+func updateSubscription(w http.ResponseWriter, r *http.Request) {
+	// code to update a subscription
+}
+
+func deleteSubscription(w http.ResponseWriter, r http.Request) {
+	// code to delete a subscription
+}
+
+func runMigrations() {
+	m, err := migrate.New(
+		"file://db/migrations",
+		"postgres://tomeknawrocki@localhost/subscription_crud?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Migration up error: %v", err)
+	}
+	log.Println("Migrations applied successfully!")
+}
+
 func main() {
+	var err error
+	db, err = connectToDatabase()
+	if err != nil {
+		log.Fatalf("Database connection error: %v", err)
+	}
+
+	log.Println("Connected to database successfully!")
+	runMigrations()
+
 	router := mux.NewRouter()
 
 	subscriptionRoutes := router.PathPrefix("/api/v1/subscriptions").Subrouter()
